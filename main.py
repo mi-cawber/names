9283a2bd647334224ae17b3fc0dcffc2fb0028ef
mi-cawber <106614898+mi-cawber@users.noreply.github.com>
2025-11-10 16:59:32 -0500

    .

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+
+# mash the 145 files into one df
+def mash():
+    # empty DataFrame
+    df = pd.DataFrame()
+
+    # raw files have no headers, need to add them
+    cols = ['Name', 'Sex', 'Frequency']
+
+    # make list of years for iteration
+    yrs = np.arange(1880,2025).tolist()
+
+    # the masher
+    for yr in yrs:
+        print(f'Loading year: {yr}')
+
+        # load file into df
+        df1 = pd.read_csv('data/yob' + str(yr) + '.txt', header=None)
+
+        # add cols to df
+        df1.columns = cols
+
+        # add year column to identify when mashed
+        df1['Year'] = yr
+        
+        # mash onto master df
+        df = pd.concat([df,df1])
+
+    #return final result
+    return df
